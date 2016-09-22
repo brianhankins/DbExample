e9633e19cd72312f80667087c426d317451f12a3
NickolasAcosta <NickAcosta@outlook.com>
2016-09-21 20:37:04 -0500

    the bad way

---
+++ WebApplication2/WebApplication2/Controllers/PersonController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    [RoutePrefix("api/person")]
+    public class PersonController : ApiController
+    {
+        [HttpGet]
+        [Route("{name}")]
+        public List<Person> Test()
+        {
+            var people = new List<Person>();
+
+            var connectionString = "";
+            var connection = new SqlConnection(connectionString);
+            var sql = "SELECT * FROM testdb..Persons";
+
+            var command = new SqlCommand(sql, connection);
+            command.CommandType = CommandType.Text;
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var firstName = reader["firstName"].ToString();
+                var lastName = reader["lastName"].ToString();
+                var ageOfPerson = (int)reader["age"];
+                var isCool = (bool)reader["isCool"];
+                var person = new Person(firstName, lastName, ageOfPerson, isCool);
+                people.Add(person);
+            }
+            return people;
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Person
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public int Age { get; }
+        public bool IsCool { get; }
+
+        public Person(string firstName, string lastName, int age, bool isCool)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            IsCool = isCool;
+        }
+
+    }
+
+    
+}
